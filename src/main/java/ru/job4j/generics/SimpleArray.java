b93cb3b63c51bc1f0b0c5e21855e1523f2a7d51e
BogdanKopylov <bogdankopylov@gmail.com>
2021-03-19 14:32:47 +0200

    5.2.1. Реализовать SimpleArray<T> [#492801]. Исправлен метод remove().

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -27,6 +27,13 @@ public class SimpleArray<T> implements Iterable<T> {
     }
     public void remove(int index) {
         Objects.checkIndex(index, count);
+        if (index < count - 1) {
+            System.arraycopy(array, (index + 1), array, index, count - 1 - index);
+        }
+            array[count - 1] = null;
+            count--;
+        }
+/*
         if (index == 0) {
             System.arraycopy(array, 1, array, 0, count - 1);
             array[count - 1] = null;
@@ -38,7 +45,8 @@ public class SimpleArray<T> implements Iterable<T> {
             System.arraycopy(array, (index + 1), array, index, count - 1 - index);
             count--;
         }
-    }
+*/
+
     public T get(int index) {
         Objects.checkIndex(index, count);
         return this.array[index];
