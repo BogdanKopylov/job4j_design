bf7522c6c964ac2cc78ab3b50fef6b85253dacb1
BogdanKopylov <bogdankopylov@gmail.com>
2021-06-01 18:17:37 +0300

    0.1. FileOutputStream. [#492828]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,18 +1,21 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LogFilter {
+    final static int STATUS_POSITION = 8;
+
     public static List<String> filter(String file) {
         try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
-            return in.lines().filter(e -> e.split(" ")[8].equals("404"))
+            return in.lines().filter(e -> e.split(" ")[STATUS_POSITION].equals("404"))
                     .collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) {
